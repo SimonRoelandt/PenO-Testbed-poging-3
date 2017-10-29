53e502b1108777b26db1a68be57be4c6947acfbc
gillis <gillis@10.32.48.176>
2017-10-29 18:51:45 +0100

    Vlieg testen

--- src/game/DummyGame.java
+++ src/game/DummyGame.java
@@ -81,7 +81,7 @@ public class DummyGame implements IGameLogic {
         gameItem3.setPosition(-1, -1, -3);
         gameItem3.setRotation(34f, 53f, 45f);
         gameItem2.setPosition(1, -2, -5);
-        gameItem.setPosition(0, 0, -50);
+        gameItem.setPosition(0, -5, -50);
         gameItem.setRotation(-60f, 20f, 40f);
         gameItems = new GameItem[] { gameItem, droneItem};
 
@@ -147,7 +147,7 @@ public class DummyGame implements IGameLogic {
         
     
         //Vlieg recht door + gravitatie
-        System.out.println("Pos: " + drone.getPos());
+        //System.out.println("Pos: " + drone.getPos());
         //System.out.println("Vel: " + drone.getVelocity());
         //drone.setVelocity(drone.getNewVelocity(timer.getElapsedTime()));
         droneItem.setPosition(drone.getXPos(), drone.getYPos(), drone.getZPos());
@@ -160,10 +160,10 @@ public class DummyGame implements IGameLogic {
         
         
         //bepaalt wanneer de simulatie stopt
-//        if (drone.getZPos() - gameItems[0].getPosition().z < 1f) {
-//        	System.out.println(timer.getTot() + "end");
-//        	window.simulationEnded = true;
-//        }
+        if ((drone.getZPos() - gameItems[0].getPosition().z < 1f)&&(drone.getYPos() - gameItems[0].getPosition().y < 1f)&&(drone.getXPos() - gameItems[0].getPosition().x < 1f) ) {
+       	System.out.println(timer.getTot() + "end");
+        	window.simulationEnded = true;
+        }
         if (timer.getTot() > 2000) {
         	System.out.println(timer.getTot());
         	window.simulationEnded = true;
