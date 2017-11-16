0a8fe65caa8c9cb1efc2bec0e5b3d8608bebfa0c
James <james.defauw@gmail.com>
2017-11-16 12:04:17 +0100

    Restructuting the Engine to the DronePart Superclass

--- src/drone/Engine.java
+++ src/drone/Engine.java
@@ -1,66 +1,46 @@
 package drone;
 
 import org.lwjgl.util.vector.Vector3f;
-
-import fysica.Fysica;
-
-public class Engine implements DroneObject{
+public class Engine extends DronePart{
 
 	private float thrust;
-	private float mass;
-	private Vector3f gravation;
-	private Vector3f position;
-	private float maxThrust; //Misschien nog aanpassen
+	private static final float maxThrust= 5000;
 	
-	private Fysica fysica;
-	
-	public Engine(double thrust, double mass, Vector3f pos) {
+	public Engine(
+			double thrust, 
+			double mass, 
+			Vector3f relativePosition) {
+		
 		this.thrust = (float) thrust;
 		this.mass = (float) mass;
-		this.position = pos;
-		fysica = new Fysica();
-		this.gravation = fysica.gravitationForce(this);
-		this.maxThrust = 5000; //Misschien nog aanpassen
+		
+		this.setRelativePosition(relativePosition);
 	}
 	
-	public Vector3f getThrust() {
-		Vector3f v1 = new Vector3f(0, 0 ,(float) - this.thrust);
+	private Vector3f getThrust() {
+		Vector3f v1 = new Vector3f(0, 0 ,(float) - this.getThrustScalar());
 		return v1;
+	}
+	
+	@Override
+	public Vector3f getDronePartForce() {
+		return this.getThrust();
 	}
 	
 	public float getThrustScalar() {
 		return this.thrust;
 	}
 	
-	public float getMass() {
-		return this.mass;
-	}
-	
-	@Override
-	public Vector3f getTotalForce() {
-		return fysica.totalForce(this);
-	}
-
-	@Override
-	public Vector3f getGraviation() {
-		return fysica.gravitationForce(this); 
-	}
-	
-	public void setPos(Vector3f pos) {
-		this.position = pos;
-	}
-	
-	public Vector3f getPos() {
-		return this.position;
-	}
 
 	public void setThrust(float thrust) {
 		this.thrust = thrust;
 	}
-	
-	public float getMaxThrust() {
-		return this.maxThrust;
-	}
 
+	public static float getMaxthrust() {
+		return maxThrust;
+	}
+	
+
+	
 	
 }
